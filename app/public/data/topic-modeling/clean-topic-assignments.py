04056aa86f290407419806947a9b2db4c9bd46e9
Caroline Tran <carolinetran.sea@gmail.com>
2024-11-27 17:31:51 -0800

    cleaned topic assignment, added text labels

--- app/public/data/topic-modeling/clean-topic-assignments.py
+++ app/public/data/topic-modeling/clean-topic-assignments.py
@@ -1,14 +1,24 @@
 import json 
 
-# import the old file 
+# import the topic assignments file (the old cluttered one) 
 with open("./results/top_topics_with_weights.json", "r") as assignmentsFile:
   topicAssignments = json.load(assignmentsFile)
 # print(topicAssignments)
 
-# create json structure 
+# import the topic ref file (will use to get all the text labels from it)
+with open("./results/topics_NMF_15.json", "r") as topicsRefFile:
+  topicsRef = json.load(topicsRefFile)
+# print(topicsRef)
+
+# create json structure for cleaned topic assignments
 assignmentsDict = {}
 for post in topicAssignments:
-  assignmentsDict[post["post_id"]] = post["topics"]["first"]["topic"]
+  postIdx = post["topics"]["first"]["topic"]
+  postInfo = {
+    "idx": postIdx,
+    "label": topicsRef[postIdx]["label"]
+  }
+  assignmentsDict[post["post_id"]] = postInfo
 print(assignmentsDict)
 print("number of total letters in dictionary: ", len(assignmentsDict))
 
