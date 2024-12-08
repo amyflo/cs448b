4981832ecc5ecb35070c63d858df584253287bb0
Caroline Tran <carolinetran.sea@gmail.com>
2024-12-08 00:54:06 -0800

    saving so i can merge from main

--- app/src/app/components/visualizations/p2/topic-modeling.js
+++ app/src/app/components/visualizations/p2/topic-modeling.js
@@ -80,28 +80,6 @@ const TSNEVisualization = ({
     const chartLeftMargin = 160;
     const chartContainerW = 925;
     const chartContainerH = 600;
-<<<<<<< HEAD
-=======
-
-    // define the color theme (15 colors for 15 topics)
-    const colors = [
-      "#e6194B",
-      "#f58231",
-      "#FFDB58",
-      "#bfef45",
-      "#3cb44b",
-      "#4363d8",
-      "#42d4f4",
-      "#911eb4",
-      "#f032e6",
-      "#808000",
-      "#8a9edb",
-      "#9A6324",
-      "#000075",
-      "#a9a9a9",
-      "#ffd8b1",
-    ];
->>>>>>> main
 
     // Select the SVG element chart and tooltip (these are the things that should be loading the plots)
     const chartSVG = d3.select(`#${id}-chart`);
